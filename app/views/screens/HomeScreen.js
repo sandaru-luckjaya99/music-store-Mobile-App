46469eee4887a29ceef5ac7eba5c14b6c1f2eddc
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2021-11-17 22:41:22 +0530

    categories with - after select border

--- app/views/screens/HomeScreen.js
+++ app/views/screens/HomeScreen.js
@@ -8,9 +8,16 @@ import COLORS from './colors' ;
 const HomeScreen = () => {
 
   const categories =['Strings', 'keyboard','Wind','purcusion']
+  const [categoryIndex,setCategoryIndex] = React.useState(0)
 
   const CategoryList = () => {
-    return <View style ={style.categorieContainer}></View>
+    return( 
+      <View style ={style.categoryContainer}>
+        {categories.map((item,index)=> (
+        <Text key = {[index]} style={[style.categoryText,categoryIndex == index && style.categoryTextSelected ]}>{item}</Text>
+      ))}
+      </View>
+    )  
   };
 
   return (
@@ -21,6 +28,7 @@ const HomeScreen = () => {
       backgroundColor: COLORS.white,
     }}>
       <View style={style.header}>
+        {/* Welcome text with name */}
         <View>
           <Text style={{fontSize:25,fontWeight:'bold' , color:COLORS.blue_light}}>Welcome to</Text>
           <Text style={{fontSize:35, fontWeight:'bold',color:COLORS.blue_thick}}>Soul Music</Text>
@@ -28,6 +36,7 @@ const HomeScreen = () => {
         <Icon name='shopping-cart' size={25} color={COLORS.blue_light} marginTop={30}></Icon>   
       </View>
 
+      {/* Search bar and filter */}
       <View style={{marginTop:15 , flexDirection : 'row'}}>
         <View style={style.searchContainer}>
           <Icon name="search" size={20} style={{marginLeft: 20}}></Icon>
@@ -38,7 +47,7 @@ const HomeScreen = () => {
         </View>
       </View>
 
- 
+      <CategoryList/>
     </SafeAreaView>
   );
 };
@@ -71,6 +80,24 @@ const style = StyleSheet.create({
     justifyContent : 'center',
     alignItems : 'center',
     borderRadius : 10,
+  },
+  categoryContainer:{
+    flexDirection: 'row',
+    marginTop : 25,
+    marginBottom : 15,
+    justifyContent : 'space-between'
+  },
+  categoryText: {
+    fontSize : 16,
+    color : COLORS.blue_light,
+    fontWeight : '900'
+  },
+  categoryTextSelected : {
+    color : COLORS.blue_thick,
+    paddingBottom : 5,
+    borderBottomWidth : 2,
+    borderColor:COLORS.blue_thick
+
 
   }
 });
