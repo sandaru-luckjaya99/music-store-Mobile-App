edf3c4854b8d6f5d252c4541435e1f6fc225c1c6
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2021-11-17 21:46:01 +0530

    color added

--- app/views/screens/colors.js
+++ app/views/screens/colors.js
@@ -1,9 +1,13 @@
 const COLORS = {
-  white: '#fff',
-  dark: '#000',
-  red: '#F52A2A',
-  light: '#F1F1F1',
-  green: '#00B761',
+  white : '#fff',
+  dark : '#000',
+  red : '#F52A2A',
+  light : '#F1F1F1',
+  green : '#00B761',
+  blue_thick :'#3b5998',
+  blue_light : '#8b9dc3',
+  ash : '#dfe3ee',
+  
 };
 
 export default COLORS;
