ec0aa701c5662b1b48d23884da53ed4c290c2498
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2021-11-17 21:44:18 +0530

    invisible header

--- app/Root.js
+++ app/Root.js
@@ -13,23 +13,36 @@
  import DetailsScreen from './views/screens/DetailsScreen';
  
  import {StatusBar} from 'react-native';
-//  import COLORS from './src/constss/colors';
+ import COLORS from './views/screens/colors'
  import {NavigationContainer} from '@react-navigation/native';
  
  // screens
  
- 
- 
- 
- 
- 
  const App = () => {
    return (
      <NavigationContainer >
-       <StatusBar barStyle = 'dark-content'>
-       </StatusBar>
+       <StatusBar barStyle = 'dark-content'/>
        <Stack.Navigator  ScreenOptions={{header : () => null}}>
-         <Stack.Screen name="Home" component={HomeScreen} />
+         <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{
+            headerShown : false,
+            
+            // title:'Home',
+            // headerStyle: {   
+            //   backgroundColor: COLORS.ash, 
+              
+            // },
+            // headerTintColor: COLORS.light,
+            // headerTitleStyle: {
+            //   flex: 1,
+            //   fontWeight: 'bold',
+            //   fontSize : 25,
+              
+            // },
+          }}
+         />
          <Stack.Screen name="Details" component={DetailsScreen} />
        </Stack.Navigator>
      </NavigationContainer>
