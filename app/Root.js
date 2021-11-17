6ccca080a3d7597333ce60750dcc7938c80c3df8
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2021-11-17 12:05:55 +0530

    root with navigators

---
+++ app/Root.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import {createStackNavigator} from '@react-navigation/stack';
+ const Stack = createStackNavigator();
+ import HomeScreen from './views/screens/HomeScreen';
+ import DetailsScreen from './views/screens/DetailsScreen';
+ 
+ import {StatusBar} from 'react-native';
+//  import COLORS from './src/constss/colors';
+ import {NavigationContainer} from '@react-navigation/native';
+ 
+ // screens
+ 
+ 
+ 
+ 
+ 
+ 
+ const App = () => {
+   return (
+     <NavigationContainer >
+       <StatusBar barStyle = 'dark-content'>
+       </StatusBar>
+       <Stack.Navigator  ScreenOptions={{header : () => null}}>
+         <Stack.Screen name="Home" component={HomeScreen} />
+         <Stack.Screen name="Details" component={DetailsScreen} />
+       </Stack.Navigator>
+     </NavigationContainer>
+   );
+ };
+ 
+ export default App;
