b07c613a9ffd8adbbe779891d09d303708d84c12
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2021-11-25 18:22:28 +0530

    root updated

--- app/Root.js
+++ app/Root.js
@@ -6,47 +6,34 @@
  * @flow strict-local
  */
 
- import React from 'react';
- import {createStackNavigator} from '@react-navigation/stack';
- const Stack = createStackNavigator();
- import HomeScreen from './views/screens/HomeScreen';
- import DetailsScreen from './views/screens/DetailsScreen';
- 
- import {StatusBar} from 'react-native';
- import COLORS from './views/screens/colors'
- import {NavigationContainer} from '@react-navigation/native';
- 
- // screens
- 
- const App = () => {
-   return (
-     <NavigationContainer >
-       <StatusBar barStyle = 'dark-content'/>
-       <Stack.Navigator  ScreenOptions={{header : () => null}}>
-         <Stack.Screen 
-          name="Home" 
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+const Stack = createStackNavigator();
+import HomeScreen from './views/screens/HomeScreen';
+import DetailsScreen from './views/screens/DetailsScreen';
+
+import {StatusBar} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+
+// screens
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" />
+      <Stack.Navigator ScreenOptions={{header: () => null}}>
+        <Stack.Screen
+          name="Home"
           component={HomeScreen}
           options={{
-            headerShown : false,
-            
-            // title:'Home',
-            // headerStyle: {   
-            //   backgroundColor: COLORS.ash, 
-              
-            // },
-            // headerTintColor: COLORS.light,
-            // headerTitleStyle: {
-            //   flex: 1,
-            //   fontWeight: 'bold',
-            //   fontSize : 25,
-              
-            // },
+            headerShown: false,
           }}
-         />
-         <Stack.Screen name="Details" component={DetailsScreen} />
-       </Stack.Navigator>
-     </NavigationContainer>
-   );
- };
- 
- export default App;
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
