1a053c789e7265e6843979cf1de36042d78c7a47
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2021-12-07 11:42:14 +0530

    all commit before install onboard navigator

--- app/views/screens/DetailsScreen.js
+++ app/views/screens/DetailsScreen.js
@@ -1,61 +1,77 @@
 import React from 'react';
 import {View, SafeAreaView, Image, Text, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import Instrument from '../../consts/instrument';
 import COLORS from './colors';
 
 const DetailsScreen = ({navigation, route}) => {
+  // const Instrument = route.params;
   const instrument = route.params;
 
   return (
     <SafeAreaView
       style={{
         flex: 1,
-        backgroundColor: COLORS.white,
+        backgroundColor:COLORS.white,
       }}>
+      {/* header */}
       <View style={style.header}>
         <Icon name="arrow-back" size={28} onPress={() => navigation.goBack()} />
-        <Icon name="shopping-cart" size={28} />
+        <Icon name="shopping-cart" size={28} color={COLORS.blue_light} size={35} />
       </View>
+
+      {/* Immage container */}
       <View style={style.imageContainer}>
         <Image source={instrument.img} style={{resizeMode: 'contain', flex: 1}} />
       </View>
+
+      {/* Detail container */}
       <View style={style.detailsContainer}>
-        <View
-          style={{
-            marginLeft: 20,
-            flexDirection: 'row',
-            alignItems: 'flex-end',
-          }}>
-          <View style={style.line} />
-          <Text style={{fontSize: 18, fontWeight: 'bold'}}>Best choice</Text>
-        </View>
+
+        {/* -- Instrument name */}
         <View
           style={{
             marginLeft: 20,
             marginTop: 20,
             flexDirection: 'row',
+            alignItems: 'flex-end',
+          }}>
+          <Text style={{fontSize: 28, fontWeight: 'bold', color : COLORS.blue_thick}}>{instrument.name}</Text>
+        </View>
+
+        {/* -- Instrument Details */}
+        <View
+          style={{
+            marginLeft: 20,
+            marginTop: 2,
+            flexDirection: 'row',
             justifyContent: 'space-between',
             alignItems: 'center',
           }}>
-          <Text style={{fontSize: 22, fontWeight: 'bold'}}>{instrument.name}</Text>
-          <View style={style.priceTag}>
+          <Text style={{fontSize: 15, fontWeight: 'bold', color : COLORS.blue_thick}}>{instrument.size_or_type}</Text>
+          
+          {/* --- Instrument Price  */}
+          {/* <View style={style.priceTag}>
             <Text
               style={{
                 marginLeft: 15,
                 color: COLORS.white,
                 fontWeight: 'bold',
-                fontSize: 16,
+                fontSize: 20,
               }}>
-              ${instrument.price}
+              LKR{instrument.price}
             </Text>
-          </View>
+          </View> */}
         </View>
-        <View style={{paddingHorizontal: 20, marginTop: 10}}>
-          <Text style={{fontSize: 20, fontWeight: 'bold'}}>About</Text>
+
+        {/* -- Instrument content */}
+        <View style={{paddingHorizontal: 20, marginTop: 2}}>
+          <Text style={{fontSize: 15, fontWeight: 'bold',color : COLORS.blue_thick}}>{instrument.content}</Text>
           <Text
             style={{
-              color: 'grey',
+              color:COLORS.blue_thick,
               fontSize: 16,
+              fontWeight : 'bold',
               lineHeight: 22,
               marginTop: 10,
             }}>
@@ -63,7 +79,7 @@ const DetailsScreen = ({navigation, route}) => {
           </Text>
           <View
             style={{
-              marginTop: 20,
+              marginTop: 100,
               flexDirection: 'row',
               justifyContent: 'space-between',
             }}>
@@ -80,6 +96,7 @@ const DetailsScreen = ({navigation, route}) => {
                   fontSize: 20,
                   marginHorizontal: 10,
                   fontWeight: 'bold',
+                  color : COLORS.blue_thick
                 }}>
                 1
               </Text>
@@ -87,7 +104,6 @@ const DetailsScreen = ({navigation, route}) => {
                 <Text style={style.borderBtnText}>+</Text>
               </View>
             </View>
-
             <View style={style.buyBtn}>
               <Text
                 style={{color: COLORS.white, fontSize: 18, fontWeight: 'bold'}}>
@@ -102,6 +118,7 @@ const DetailsScreen = ({navigation, route}) => {
 };
 
 const style = StyleSheet.create({
+
   header: {
     paddingHorizontal: 20,
     marginTop: 20,
@@ -109,37 +126,57 @@ const style = StyleSheet.create({
     justifyContent: 'space-between',
   },
   imageContainer: {
-    flex: 0.36,
+    flex: 0.40,
     marginTop: 20,
     justifyContent: 'center',
     alignItems: 'center',
+    //backgroundColor: COLORS.bluueLi,
+    marginBottom: 0,
+    borderBottomLeftRadius: 100, 
+    // backgroundColor:COLORS.ash,
+    // borderRadius: 20,
+    //paddingTop: 30,
+    // borderTopStartRadius:20,
+    // borderBottomStartRadius : 20,
+    // borderTopRightRadius : 90,
+    // borderBottomRightRadius : 90,
+    // marginHorizontal: 5,
+    // borderBottomRightRadius: 100, 
+    
+    
   },
   detailsContainer: {
-    flex: 0.25,
-    backgroundColor: COLORS.light,
-    marginHorizontal: 5,
-    marginBottom: 5,
-    borderRadius: 20,
-    marginTop: 30,
-    paddingTop: 30,
+    flex: 0.40,
+    backgroundColor: COLORS.bluueLi,
+    marginHorizontal:10 ,
+    marginBottom:5,
+    // borderRadius: 20,
+    marginTop: 8,
+    paddingTop: 20,
+    borderTopLeftRadius: -100,
+    //borderRadius : 10,
+    borderBottomLeftRadius: 85, 
+    borderTopRightRadius : 85
+    
   },
   line: {
     width: 25,
     height: 2,
-    backgroundColor: COLORS.dark,
+    backgroundColor: COLORS.blue_thick,
     marginBottom: 5,
     marginRight: 3,
   },
   borderBtn: {
-    borderColor: 'grey',
-    borderWidth: 1,
-    borderRadius: 5,
+
+    borderWidth: 2,
+    borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
     width: 60,
     height: 40,
+    borderColor : COLORS.blue_light
   },
-  borderBtnText: {fontWeight: 'bold', fontSize: 28},
+  borderBtnText: { fontWeight: 'bold', fontSize: 25, color : COLORS.blue_thick },
   buyBtn: {
     width: 130,
     height: 50,
@@ -150,8 +187,9 @@ const style = StyleSheet.create({
   },
   priceTag: {
     backgroundColor: COLORS.blue_thick,
-    width: 80,
-    height: 40,
+    
+    width: 120,
+    height: 49,
     justifyContent: 'center',
     borderTopLeftRadius: 25,
     borderBottomLeftRadius: 25,
