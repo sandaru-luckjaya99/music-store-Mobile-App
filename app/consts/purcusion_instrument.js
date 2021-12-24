c5b2478ed73b21b09bc4556c1befb87a432f3bc7
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2021-12-24 10:04:08 +0530

    const for category(new edition)

---
+++ app/consts/purcusion_instrument.js
@@ -0,0 +1,65 @@
+const Instrumentt = [
+  {
+    id: 1,
+    name: 'lark - violin',
+    size_or_type :'Size4/4',
+    content : "With a bow",
+    other :"With a fiber violin case",
+    price: '39,000',
+    like: true,
+    img: require('../assets/larkviolin.png'),
+    prewImg1:require('../assets/larkviolinBow.png'),
+    prewImg2:require('../assets/larkviolinCase.png'),
+    about:
+      'Great Violin For Beginners.The set includes all the necessities to start learning how to play',
+    
+  },
+
+  {
+    id: 2,
+    name: 'Cremona violin',
+    size_or_type :'Size - 4/4',
+    content : "Without a bow",
+    other :"With a fiber violin case",
+    price: '29,000',
+    like: false,
+    img: require('../assets/cremonaviolin.png'),
+    prewImg1:require('../assets/cremonaviolinBow.png'),
+    prewImg2:require('../assets/cremonaviolinCase.png'),
+    about:
+      'Great Violin For Beginners.The set includes all the necessities to start learning how to play',
+  },
+  {
+    id: 3,
+    name: 'Yamaha f3-10',
+    size_or_type :'Size - 4/4',
+    content : "with fiber case",
+    other :"with a branded string set",
+    price: '25,000',
+    like: false,
+    img: require('../assets/f310yamaha.png'),
+    prewImg1:require('../assets/f310yamahaCase.png'),
+    prewImg2:require('../assets/f310yamahaStrings.png'),
+    about:
+      'Great Violin For Beginners.The set includes all the necessities to start learning how to play',
+  },
+
+  {
+    id: 4,
+    name: 'Yamaha c-30',
+    size_or_type :'Size - 4/4',
+    content : "With a hard case",
+    other : "with a guitar capo",
+    price: '24,000',
+    like: true,
+    img: require('../assets/c30yamaha.png'),
+    prewImg1:require('../assets/cremonaviolinBow.png'),
+    prewImg2:require('../assets/cremonaviolinCase.png'),
+    about:
+      'Great classical guitar For Beginners.The set includes all the necessities to start your professional playing',
+  },
+
+
+];
+
+export default Instrumentt;
